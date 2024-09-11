dda9b64bfb122766e757b4376c101fa1dafc0d07
Prabhakar Kumar <prabhakarkumar16181@gmail.com>
2024-09-11 18:55:13 +0300

    Taking skills data from a JSON file and using the map method on it.

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -1,55 +1,27 @@
 import "./css/skills.css"
 
 function Skills() {
+
+    const skillsData = require('../data.json')
+
   return (
     <section id="skills">
         <div className="container">
             <h1 className="sectionTitle">My <span>Skills</span></h1>
             <p className="sectionHeadline">My Expertise</p>
             <div className="skillShowContainer">
-                <div className="box">
-                    <div><i style={{color:"#e34c26"}} class="fa-brands fa-html5"></i></div>
-                    <div>
-                        <h1>HTML</h1>
-                        <p>Experienced</p>
-                    </div>
-                </div>
-                <div className="box">
-                    <div><i style={{color:"#264de4"}} class="fa-brands fa-css3-alt"></i></div>
-                    <div>
-                        <h1>CSS</h1>
-                        <p>Experienced</p>
-                    </div>
-                </div>
-                <div className="box">
-                    <div><i style={{color:"#fce300"}} class="fa-brands fa-square-js"></i></div>
-                    <div>
-                        <h1>JavaScript</h1>
-                        <p>Intermediate</p>
-                    </div>
-                    
-                </div>
-                <div className="box">
-                    <div><i style={{color:"#1ABCFE"}} class="fa-brands fa-figma"></i></div>
-                    <div>
-                        <h1>Figma</h1>
-                        <p>Intermediate</p>
-                    </div>
-                </div>
-                <div className="box">
-                    <div><i style={{color:"#00aeff"}} class="fa-brands fa-react"></i></div>
-                    <div>
-                        <h1>React js</h1>
-                        <p>Intermediate</p>
-                    </div>
-                </div>
-                <div className="box">
-                    <div><i style={{color:"#f1502f"}} class="fa-brands fa-git-alt"></i></div>
-                    <div>
-                        <h1>GIT</h1>
-                        <p>Basic</p>
-                    </div>
-                </div>
+                {
+                    skillsData.skillData.map((item,index)=>(
+                        <div className="box">
+                            <div><i style={{color:item.color}} class={item.iconClass}></i></div>
+                            <div>
+                                <h1>{item.skillName}</h1>
+                                <p>{item.skillStatus}</p>
+                            </div>
+                        </div>
+                    ))
+                }
+                
             </div>
         </div>
     </section>
