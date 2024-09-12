2046da3597370e6c0822445453dbb40154c64117
Prabhakar Kumar <prabhakarkumar16181@gmail.com>
2024-09-12 18:13:36 +0300

    comments

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,11 +4,16 @@ import profileImage from "./image/main-image.png"
 function Home() {
   return (
     <div id="home" className="home">
+        {/* Web Name */}
         <p className="headerName" style={{color:"white"}}>Pr<span style={{color:"#0891B2"}}>abhak</span>ar</p>
+
         <div className="container">
+
+            {/* Home Image */}
             <div className="image">
                 <img src={profileImage} width="100%" alt="" />
             </div>
+
             <div className="home-content">
                 <p className="hellowText">Hello!</p>
                 <p className="nameText">My Name is Prabhakar</p>
@@ -36,6 +41,7 @@ function Home() {
                     
                 </div>
             </div>
+            
         </div>
     </div>
   )
