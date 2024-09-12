f7de48fa3e0d534be3e172d4e751c5a9808cfdef
Prabhakar Kumar <prabhakarkumar16181@gmail.com>
2024-09-12 15:20:27 +0300

    style updated

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,5 +1,5 @@
-import "./css/about.css"
-import aboutImage from "./image/about-profile-image.png"
+import "./css/about.css";
+import aboutImage from "./image/about-profile-image.png";
 
 function About() {
   return (
@@ -7,28 +7,23 @@ function About() {
         <div className="container">
             <h1 className="sectionTitle">About <span>Me</span></h1>
             <h3 className="sectionHeadline">My introduction</h3>
-            <div className="about-container">
-                <div className="aboutContent">
-                    <p><img className="floatAboutImg" src={aboutImage} width="350px" alt="aboutImage" />My name is Prabhakar Kumar. I am a Frontend Developer. When I passed 10th, I became interested in App Development due to which I started learning Programming Languages. First of all I started learning Java language. After learning it till intermediate I came to know that Google has created a language called Kotlin for App Development and then I started learning Kotlin. I learned about the basics needed to develop an App and then created one or two smaller Apps. Later I chose web development for a few reasons. After learning HTML, CSS and JavaScript, I also created 5-6 projects. After that came my 12th final exams due to which I stayed away from Web Development for a few months. After the exams were over, I moved ahead with my Web Development journey.<br/><br/>
-                    <div>
-                        <div className="box">
-                            <h2><i style={{color:"#00ccff"}} class="fa-solid fa-award"></i> &nbsp; Experience</h2>
-                            <p>02+ Years</p>
-                        </div>
-                        <div className="box">
-                            <h2><i style={{color:"#00ccff"}} class="fa-solid fa-book-open-reader"></i> &nbsp; Education</h2>
-                            <p>BCA Bachelors Degree</p>
-                        </div>
+            <div className="about-content">
+                <p>My name is Prabhakar Kumar. I am a Frontend Developer. When I passed 10th, I became interested in App Development due to which I started learning Programming Languages. First of all I started learning Java language. After learning it till intermediate I came to know that Google has created a language called Kotlin for App Development and then I started learning Kotlin. I learned about the basics needed to develop an App and then created one or two smaller Apps. Later I chose web development for a few reasons. After learning HTML, CSS and JavaScript, I also created 5-6 projects. After that came my 12th final exams due to which I stayed away from Web Development for a few months. After the exams were over, I moved ahead with my Web Development journey.</p>
+                <div>
+                    <div className="box">
+                        <h2><i style={{color:"#00ccff"}} class="fa-solid fa-award"></i> &nbsp; Experience</h2>
+                        <p>02+ Years</p>
                     </div>
-                    </p>
+                    <div className="box">
+                        <h2><i style={{color:"#00ccff"}} class="fa-solid fa-book-open-reader"></i> &nbsp; Education</h2>
+                        <p>BCA Bachelors Degree</p>
+                    </div>
                 </div>
-                <div className="aboutImage">
-                    <img src={aboutImage} width="100%" alt="aboutImage" />
-                </div>
+                <img className="aboutImage" src={aboutImage} alt="aboutImage" />
             </div>
         </div>
     </section>
   )
 }
 
-export default About
+export default About;
